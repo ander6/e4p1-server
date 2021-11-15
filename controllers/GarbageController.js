7a883633c565ccf755fa6547125d72015a6b20e7
ander6 <anderunaidel@gmail.com>
2021-11-15 10:11:23 +0100

    (NEW) Garbage model created and intregrated getAllGarbages to show all data.

--- controllers/GarbageController.js
+++ controllers/GarbageController.js
@@ -1,1 +1,8 @@
 import GarbageModel from "../models/garbageModel.js";
+export const getAllGarbages = (req,res) => {
+    GarbageModel.find({}, (err,garbages) => {
+        if(err) return res.status(500).send({message: `Error al realizar la petición: ${err}`});
+        if(!garbages) return res.status(404).send({message: `No existen garbages`});
+        res.status(200).send({garbages: garbages});
+    });
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,15 @@
 import  Express  from "express";
+
 const router = Express.Router();
-import { getUserById,getAllUsers,insertData,updateData, deleteData} from "../controllers/UserController.js";
+import { getUserById, getAllUsers, insertData, updateData, deleteData} from "../controllers/UserController.js";
+import { getAllGarbages} from "../controllers/GarbageController.js";
 router.get('/users/',getAllUsers);
 router.get('/users/:id',getUserById);
 router.post('/users/',insertData);
 router.put('/users/:id',updateData);
 router.delete('/users/:id',deleteData);
+
+router.get('/garbages/',getAllGarbages);
+
+
 export default router;
