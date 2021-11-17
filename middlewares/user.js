07cd590cb42510a0ec88a8cd6138421e18cae022
Misil4 <carlos.silva@ikaslea.aeg.eus>
2021-11-17 20:33:17 +0100

    (UPDATE) If user exist and rol conditionalds added to insert

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,5 +1,5 @@
 import validator from 'validator';
 export default function validationMiddleware(req, res, next) {
-    	if (validator.isEmail(req.body.email)) return next();
+    	if (validator.isEmail(req.body.user.email)) return next();
     	res.status(400).send({message: `Email inválido`});
 };
